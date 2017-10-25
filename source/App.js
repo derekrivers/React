79cff322bedffdeb8e56bbcc6cf7e983fbbde1ad
DerekRivers <dyk3rz@googlemail.com>
2017-10-25 15:24:32 +0100

    added state mgmt and api calls.

--- source/App.js
+++ source/App.js
@@ -1,39 +1,4 @@
 import React, {Component} from 'react';
-import KanbanBoard from './KanbanBoard';
+import KanbanBoardContainer from './KanbanBoardContainer';
 
-let cardsList = [
-    {
-        id : 1,
-        title: "This is a new card with a very very lobg title, thus having more than 80 characters my freinds.",
-        description :"I should read the **whole** book",
-        color: '#BD8D31',
-        status: "in-progress",
-        tasks:[]
-    },
-    {
-        id : 2,
-        title: "Write some code",
-        description :"Code along with the samples in the book. The complete source can be found at [github](https://github.com/proreact)",
-        color: '#3A7E28',
-        status: "todo",
-        tasks:[
-            {
-                id:1,
-                name: "ContactList Example",
-                done: true
-            },
-            {
-                id:2,
-                name: "Kanban Example",
-                done: false
-            },
-            {
-                id:3,
-                name: "My own Experiments",
-                done: false
-            }
-        ]
-    }
-]
-
-React.render(<KanbanBoard cards={cardsList}/>, document.getElementById('root'));
+React.render(<KanbanBoardContainer/>, document.getElementById('root'));
--- source/KanbanBoard.js
+++ source/KanbanBoard.js
@@ -8,16 +8,19 @@ class KanbanBoard extends Component {
 
                 <List id = "todo"
                     title = "To Do"
+                    taskCallbacks = {this.props.taskCallbacks}
                     cards = {this.props.cards.filter((card) => card.status === "todo")}
                     />
                   
                 <List id = "in-progress"
                     title = "In Progress"
+                    taskCallbacks = {this.props.taskCallbacks}
                     cards = {this.props.cards.filter((card) => card.status === "in-progress")}
                     />
 
                 <List id = "done"
                     title = "Done"
+                    taskCallbacks = {this.props.taskCallbacks}
                     cards = {this.props.cards.filter((card) => card.status === "done")}
                     />   
 
@@ -28,7 +31,8 @@ class KanbanBoard extends Component {
 }
 
 KanbanBoard.propTypes = {
-    cards : PropTypes.arrayOf(PropTypes.object)
+    cards : PropTypes.arrayOf(PropTypes.object),
+    taskCallbacks : PropTypes.object
 };
 
 export default KanbanBoard;
